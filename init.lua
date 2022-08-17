fa7829f58533c7b7dd4eefd4744d71aaec729788
icedman <m4rvin2005@gmail.com>
2022-08-17 20:21:24 +0800

    cleanup with stylua; localize functions

--- init.lua
+++ init.lua
@@ -1,20 +1,23 @@
-local info = debug.getinfo(1,'S')
+local info = debug.getinfo(1, "S")
 local cpath = package.cpath
 
-package.cpath = package.cpath ..
-    ';' .. info.source:gsub('init.lua', 'build/textmate.so'):gsub('@', '') ..
-    ';' .. info.source:gsub('init.lua', '_build/textmate.so'):gsub('@', '') ..
-    ';' .. info.source:gsub('init.lua', 'textmate.so'):gsub('@', '')
+package.cpath = package.cpath
+	.. ";"
+	.. info.source:gsub("init.lua", "build/textmate.so"):gsub("@", "")
+	.. ";"
+	.. info.source:gsub("init.lua", "_build/textmate.so"):gsub("@", "")
+	.. ";"
+	.. info.source:gsub("init.lua", "textmate.so"):gsub("@", "")
 
 -- local module = require('textmate')
 
-local ok, module = pcall(require, 'textmate')
+local ok, module = pcall(require, "textmate")
 
 package.cpath = cpath
 
 if not ok then
-    -- probably need to run cmake && make
-    return
+	-- probably need to run cmake && make
+	return
 end
 
 local api = vim.api
@@ -27,184 +30,185 @@ local hl_timeout_next_tick = 0
 local hl_timeout_after_change = 150
 
 local group = api.nvim_create_augroup("textmate", { clear = true })
-local enabled = false
+local enabled = true
+local initialized = false
 
-local function setup(parameters)
+local function setup(parameters) end
+
+local function txmt_highlight_initialize()
+	local homedir = vim.fn.expand("~")
+	module.highlight_set_extensions_dir(homedir .. "/.editor/extensions/")
+	module.highlight_set_extensions_dir(homedir .. "/.vscode/extensions/")
+	module.highlight_set_extensions_dir(info.source:gsub("init.lua", "extensions/"))
+	module.highlight_load_theme("Dracula")
+	initialized = true
 end
 
-function txmt_set_language()
-    if enabled ~= true then
-        txmt_highlight_enable()
-    end
+local function txmt_set_language()
+	if enabled ~= true then
+		return false
+	end
+	if initialized ~= true then
+		txmt_highlight_initialize()
+	end
 
-    local b = api.nvim_get_current_buf()
-    if buffer_data[b] == nil then
-        buffer_data[b] = {}
-    end
+	local b = api.nvim_get_current_buf()
+	if buffer_data[b] == nil then
+		buffer_data[b] = {}
+	end
 
-    local filetype = vim.bo.filetype
-    buffer_data[b]['filetype'] = filetype
+	local filetype = vim.bo.filetype
+	buffer_data[b]["filetype"] = filetype
 
-    -- should exclude?
+	-- should exclude?
 
-    if buffer_data[b]['langid'] == nil then
-        local langid = module.highlight_load_language(vim.fn.expand('%'))
-        buffer_data[b]['langid'] = langid
-    end
+	if buffer_data[b]["langid"] == nil then
+		local langid = module.highlight_load_language(vim.fn.expand("%"))
+		buffer_data[b]["langid"] = langid
+	end
 
-    if buffer_data[b]['langid'] == -1 then
-        return
-    end
+	if buffer_data[b]["langid"] == -1 then
+		return false
+	end
 
-    module.highlight_set_language(buffer_data[b]['langid'])
-    return true
+	module.highlight_set_language(buffer_data[b]["langid"])
+	return true
 end
 
-function txmt_on_text_changed_i() 
-    local b = api.nvim_get_current_buf()
-    local r,c = unpack(vim.api.nvim_win_get_cursor(0))
-    local i = r - 1
-    local lines = api.nvim_buf_get_lines(b, i, i+1, false)
-  
-    local lc = api.nvim_buf_line_count(b)
-    local diff = 0
-    local changed_lines = 1
-    if buffer_data[b] ~= nil then
-        local last = buffer_data[b]['last_count']
-        buffer_data[b]['last_count'] = lc
-        if last ~= nil then
-          diff = lc - last
-          if diff > 0 then
-            changed_lines = diff
-            for nr=0,diff,1
-            do
-              module.highlight_add_block(nr);
-            end
-          end
-          if diff < 0 then
-            changed_lines = -diff
-            for nr=0,-diff,1
-            do
-              module.highlight_remove_block(nr);
-            end
-          end
-        end
-    end
+local function txmt_on_text_changed_i()
+	local b = api.nvim_get_current_buf()
+	local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+	local i = r - 1
+	local lines = api.nvim_buf_get_lines(b, i, i + 1, false)
 
-    for cl = 0,changed_lines,1
-    do
-      module.highlight_make_line_dirty(i+cl, b);
-    end
-    txmt_highlight_current_line(i+1, lines[1])
-    txmt_deferred_highlight_current_buffer(hl_timeout_after_change)
+	local lc = api.nvim_buf_line_count(b)
+	local diff = 0
+	local changed_lines = 1
+	if buffer_data[b] ~= nil then
+		local last = buffer_data[b]["last_count"]
+		buffer_data[b]["last_count"] = lc
+		if last ~= nil then
+			diff = lc - last
+			if diff > 0 then
+				changed_lines = diff
+				for nr = 0, diff, 1 do
+					module.highlight_add_block(nr)
+				end
+			end
+			if diff < 0 then
+				changed_lines = -diff
+				for nr = 0, -diff, 1 do
+					module.highlight_remove_block(nr)
+				end
+			end
+		end
+	end
+
+	for cl = 0, changed_lines, 1 do
+		module.highlight_make_line_dirty(i + cl, b)
+	end
+	txmt_highlight_current_line(i + 1, lines[1])
+	txmt_deferred_highlight_current_buffer(hl_timeout_after_change)
 end
 
-function txmt_on_text_changed()
-  -- txmt_on_text_changed_i()
-    if changed_timer ~= nil then
-        changed_timer:close()
-    end
+local function txmt_on_text_changed()
+	-- txmt_on_text_changed_i()
+	if changed_timer ~= nil then
+		changed_timer:close()
+	end
 
-    changed_timer = vim.defer_fn(
-        function()
-            changed_timer = nil
-            txmt_on_text_changed_i()
-        end,
-        150
-    )
+	changed_timer = vim.defer_fn(function()
+		changed_timer = nil
+		txmt_on_text_changed_i()
+	end, 150)
 end
 
-function txmt_highlight_enable()
-    local homedir = vim.fn.expand('~')
-    module.highlight_set_extensions_dir(homedir .. '/.editor/extensions/')
-    module.highlight_set_extensions_dir(homedir .. '/.vscode/extensions/')
-    module.highlight_set_extensions_dir(info.source:gsub('init.lua', 'extensions/'))
-    module.highlight_load_theme('Dracula')
-    enabled = true
+local function txmt_highlight_current_line(n, l)
+	if txmt_set_language() ~= true then
+		return
+	end
+
+	local b = api.nvim_get_current_buf()
+
+	if module.highlight_is_line_dirty(n, b) == 0 then
+		return
+	end
+
+	api.nvim_buf_clear_namespace(b, 0, n - 1, n)
+	local langid = buffer_data[b]["langid"]
+	if langid == -1 then
+		return
+	end
+	local t = module.highlight_line(l, n, langid, b)
+	for i, style in ipairs(t) do
+		local start = style[1]
+		local length = style[2]
+		local rr = style[3]
+		local gg = style[4]
+		local bb = style[5]
+		local clr = string.format("%x%x%x", rr, gg, bb)
+		api.nvim_set_hl(0, clr, { fg = "#" .. clr })
+		api.nvim_buf_add_highlight(b, 0, clr, n - 1, start, start + length)
+	end
 end
 
-function txmt_highlight_current_buffer()
-    if txmt_set_language() ~= true then
-        return
-    end
+local function txmt_highlight_current_buffer()
+	if txmt_set_language() ~= true then
+		return
+	end
 
-    local b = api.nvim_get_current_buf()
-    local lc = api.nvim_buf_line_count(b)
-    local sr = 50 -- screen rows
+	local b = api.nvim_get_current_buf()
+	local lc = api.nvim_buf_line_count(b)
+	local sr = 50 -- screen rows
 
-    local r,c = unpack(vim.api.nvim_win_get_cursor(0))
-    local ls = r - sr
-    local le = r + sr
+	local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+	local ls = r - sr
+	local le = r + sr
 
-    if ls < 0 then
-        ls = 0
-    end
-    if le > lc then
-        le = lc
-    end
+	if ls < 0 then
+		ls = 0
+	end
+	if le > lc then
+		le = lc
+	end
 
-    for i = ls, le-1, 1
-    do
-        local lines = api.nvim_buf_get_lines(b, i, i+1, false)
-        txmt_highlight_current_line(i+1, lines[1])
-    end
+	for i = ls, le - 1, 1 do
+		local lines = api.nvim_buf_get_lines(b, i, i + 1, false)
+		txmt_highlight_current_line(i + 1, lines[1])
+	end
 end
 
-function txmt_deferred_highlight_current_buffer(timeout)
-    if render_timer ~= nil then
-        render_timer:close()
-    end
+local function txmt_deferred_highlight_current_buffer(timeout)
+	if render_timer ~= nil then
+		render_timer:close()
+	end
 
-    if timeout == nil then
-        timeout = hl_default_timeout
-    end
+	if timeout == nil then
+		timeout = hl_default_timeout
+	end
 
-    render_timer = vim.defer_fn(
-        function()
-            render_timer = nil
-            txmt_highlight_current_buffer()
-        end,
-        timeout
-    )
+	render_timer = vim.defer_fn(function()
+		render_timer = nil
+		txmt_highlight_current_buffer()
+	end, timeout)
 end
 
-function txmt_highlight_current_line(n, l)
-    if txmt_set_language() ~= true then
-        return
-    end
-
-    local b = api.nvim_get_current_buf()
-
-    if module.highlight_is_line_dirty(n, b) == 0 then
-        return
-    end
-
-    api.nvim_buf_clear_namespace(b, 0, n-1, n)
-    local langid = buffer_data[b]['langid']
-    if langid == -1 then
-        return
-    end
-    local t = module.highlight_line(l, n, langid, b)
-    for i, style in ipairs(t) do
-        local start = style[1]
-        local length = style[2]
-        local rr = style[3]
-        local gg = style[4]
-        local bb = style[5]
-        local clr = string.format("%x%x%x", rr,gg,bb)
-        api.nvim_set_hl(0, clr, { fg = '#' .. clr })
-        api.nvim_buf_add_highlight(b, 0, clr, n-1, start, start + length)
-    end
+local function txmt_on_buf_enter(args)
+	-- local b = api.nvim_get_current_buf()
+	-- local lc = api.nvim_buf_line_count(b)
+	txmt_deferred_highlight_current_buffer(hl_timeout_next_tick)
 end
 
-function txmt_on_buf_enter(args) 
-    -- local b = api.nvim_get_current_buf()
-    -- local lc = api.nvim_buf_line_count(b)
-    txmt_deferred_highlight_current_buffer(hl_timeout_next_tick)
+local function txmt_on_cursor_moved(args)
+	txmt_deferred_highlight_current_buffer()
 end
 
-function txmt_on_cursor_moved(args)
-    txmt_deferred_highlight_current_buffer()
+local function txmt_on_toggle()
+	if enabled then
+		enabled = false
+	else
+		enabled = true
+	end
 end
 
 api.nvim_create_autocmd("BufEnter", { group = group, callback = txmt_on_buf_enter })
@@ -212,6 +216,12 @@ api.nvim_create_autocmd("TextChangedI", { group = group, callback = txmt_on_text
 api.nvim_create_autocmd("TextChanged", { group = group, callback = txmt_on_text_changed })
 api.nvim_create_autocmd("CursorMoved", { group = group, callback = txmt_on_cursor_moved })
 
+api.nvim_create_user_command(
+	"TextMateToggle",
+	txmt_on_toggle,
+	{ bang = true, desc = "toggle textmate syntax highlighter" }
+)
+
 return {
-    setup = setup
+	setup = setup,
 }
--- module.cpp
+++ module.cpp
@@ -2,199 +2,208 @@ extern "C" {
 #include <lua.h>
 }
 
-#define EXPORT                                                                 \
-extern "C" __attribute__((visibility("default"))) __attribute__((used))
+#define EXPORT \
+    extern "C" __attribute__((visibility("default"))) __attribute__((used))
 
+#include "grammars.h"
 #include "textmate.h"
 #include "themes.h"
-#include "grammars.h"
 #include "util.h"
 
 #include <map>
 
 std::map<int, doc_data_ptr> docs;
 
-int highlight_is_line_dirty(lua_State* L) {
-   int linenr = lua_tonumber(L, -2);
-   int docid = lua_tonumber(L, -1);
-   int block_line = linenr - 1;
+int highlight_is_line_dirty(lua_State* L)
+{
+    int linenr = lua_tonumber(L, -2);
+    int docid = lua_tonumber(L, -1);
+    int block_line = linenr - 1;
 
-   if (docs.find(docid) == docs.end()) {
-      lua_pushnumber(L, 1);
-      return 1;
-   }
+    if (docs.find(docid) == docs.end()) {
+        lua_pushnumber(L, 1);
+        return 1;
+    }
 
-   doc_data_ptr doc = docs[docid];
-   block_data_ptr block = doc->block_at(block_line);
-   if (block && !block->dirty) {
-      lua_pushnumber(L, 0);
-      return 1;
-   }
+    doc_data_ptr doc = docs[docid];
+    block_data_ptr block = doc->block_at(block_line);
+    if (block && !block->dirty) {
+        lua_pushnumber(L, 0);
+        return 1;
+    }
 
-   lua_pushnumber(L, 1);
-   return 1;
-}
-
-int highlight_make_line_dirty(lua_State* L) {
-   int linenr = lua_tonumber(L, -2);
-   int docid = lua_tonumber(L, -1);
-   int block_line = linenr - 1;
-
-   if (docs.find(docid) == docs.end()) {
-      return 1;
-   }
-
-   doc_data_ptr doc = docs[docid];
-   block_data_ptr block = doc->block_at(block_line);
-   if (block) {
-      // log(">>%d", block_line);
-      block->make_dirty();
-   }
-
-   return 1;
-}
-
-int highlight_line(lua_State* L) {
-   const char* p = lua_tostring(L, -4);
-   int linenr = lua_tonumber(L, -3);
-   int langid = lua_tonumber(L, -2);
-   int docid = lua_tonumber(L, -1);
-
-   doc_data_ptr doc;
-
-   if (docs.find(docid) == docs.end()) {
-      doc = std::make_shared<doc_data_t>();
-      docs[docid] = doc;
-   }
-
-   doc = docs[docid];
-
-
-   std::string code = p;
-   std::vector<textstyle_t> res;
-
-   int block_line = linenr - 1;
-   block_data_ptr block = doc->block_at(block_line);
-   block_data_ptr prev_block = doc->previous_block(block_line);
-   block_data_ptr next_block = doc->next_block(block_line);
-
-   res = Textmate::run_highlighter((char*)code.c_str(), 
-      Textmate::language(),
-      Textmate::theme(),
-      block ? block.get() : NULL,
-      prev_block ? prev_block.get() : NULL,
-      next_block ? next_block.get() : NULL
-      );
-
-   // log("highlight_line %d", block_line);
-
-   lua_newtable(L);
-
-   int row = 1;
-   for(auto r : res) {
-      int col = 1;
-      lua_newtable(L);
-      lua_pushnumber(L, r.start);
-      lua_rawseti(L, -2, col++);
-      lua_pushnumber(L, r.length);
-      lua_rawseti(L, -2, col++);
-      lua_pushnumber(L, r.r);
-      lua_rawseti(L, -2, col++);
-      lua_pushnumber(L, r.g);
-      lua_rawseti(L, -2, col++);
-      lua_pushnumber(L, r.b);
-      lua_rawseti(L, -2, col++);
-
-      lua_rawseti(L, -2, row++);
-   }
-
-   return 1;
-}
-
-int highlight_set_extensions_dir(lua_State* L) {
-   const char* p = lua_tostring(L, -1);
-   Textmate::initialize(p);
-   log("highlight_set_extensions_dir");
-   return 1;
-}
-
-int highlight_load_theme(lua_State* L) {
-   const char* p = lua_tostring(L, -1);
-   int theme_id = Textmate::load_theme(p);
-   log("highlight_load_theme");
-   lua_pushnumber(L, theme_id);
-   return 1;
-}
-
-int highlight_set_theme(lua_State* L) {
-   int id = lua_tonumber(L, -1);
-   Textmate::set_theme(id);
-   return 1;
-}
-
-int highlight_load_language(lua_State* L) {
-   const char* p = lua_tostring(L, -1);
-   int lang_id = Textmate::load_language(p);
-   log("highlight_load_language %s %d", p, lang_id);
-   lua_pushnumber(L, lang_id);
-   return 1;
-}
-
-int highlight_set_language(lua_State* L) {
-   int id = lua_tonumber(L, -1);
-   Textmate::set_language(id);
-   return 1;
-}
-
-int highlight_add_block(lua_State* L) {
-   int linenr = lua_tonumber(L, -2);
-   int docid = lua_tonumber(L, -1);
-
-   if (docs.find(docid) == docs.end()) {
+    lua_pushnumber(L, 1);
     return 1;
-   }
-
-  docs[docid]->add_block_at(linenr);
-  return 1;
 }
 
-int highlight_remove_block(lua_State* L) {
-   int linenr = lua_tonumber(L, -2);
-   int docid = lua_tonumber(L, -1);
+int highlight_make_line_dirty(lua_State* L)
+{
+    int linenr = lua_tonumber(L, -2);
+    int docid = lua_tonumber(L, -1);
+    int block_line = linenr - 1;
 
-   if (docs.find(docid) == docs.end()) {
+    if (docs.find(docid) == docs.end()) {
+        return 1;
+    }
+
+    doc_data_ptr doc = docs[docid];
+    block_data_ptr block = doc->block_at(block_line);
+    if (block) {
+        // log(">>%d", block_line);
+        block->make_dirty();
+    }
+
     return 1;
-   }
-
-  docs[docid]->remove_block_at(linenr);
-  return 1;
 }
 
-EXPORT int luaopen_textmate(lua_State* L) {
-   Textmate::load_theme_data(THEME_MONOKAI);
-   Textmate::load_language_data(GRAMMAR_CPP);
+int highlight_line(lua_State* L)
+{
+    const char* p = lua_tostring(L, -4);
+    int linenr = lua_tonumber(L, -3);
+    int langid = lua_tonumber(L, -2);
+    int docid = lua_tonumber(L, -1);
 
-   lua_newtable(L);
-   lua_pushcfunction(L, highlight_line);;
-   lua_setfield(L, -2, "highlight_line");
-   lua_pushcfunction(L, highlight_set_extensions_dir);
-   lua_setfield(L, -2, "highlight_set_extensions_dir");
-   lua_pushcfunction(L, highlight_load_theme);
-   lua_setfield(L, -2, "highlight_load_theme");
-   lua_pushcfunction(L, highlight_load_language);
-   lua_setfield(L, -2, "highlight_load_language");
-   lua_pushcfunction(L, highlight_set_theme);
-   lua_setfield(L, -2, "highlight_set_theme");
-   lua_pushcfunction(L, highlight_set_language);
-   lua_setfield(L, -2, "highlight_set_language");
-   lua_pushcfunction(L, highlight_is_line_dirty);
-   lua_setfield(L, -2, "highlight_is_line_dirty");
-   lua_pushcfunction(L, highlight_make_line_dirty);
-   lua_setfield(L, -2, "highlight_make_line_dirty");
+    doc_data_ptr doc;
 
-   lua_pushcfunction(L, highlight_add_block);
-   lua_setfield(L, -2, "highlight_add_block");
-   lua_pushcfunction(L, highlight_remove_block);
-   lua_setfield(L, -2, "highlight_remove_block");
-   return 1;
+    if (docs.find(docid) == docs.end()) {
+        doc = std::make_shared<doc_data_t>();
+        docs[docid] = doc;
+    }
+
+    doc = docs[docid];
+
+    std::string code = p;
+    std::vector<textstyle_t> res;
+
+    int block_line = linenr - 1;
+    block_data_ptr block = doc->block_at(block_line);
+    block_data_ptr prev_block = doc->previous_block(block_line);
+    block_data_ptr next_block = doc->next_block(block_line);
+
+    res = Textmate::run_highlighter((char*)code.c_str(),
+        Textmate::language(),
+        Textmate::theme(),
+        block ? block.get() : NULL,
+        prev_block ? prev_block.get() : NULL,
+        next_block ? next_block.get() : NULL);
+
+    // log("highlight_line %d", block_line);
+
+    lua_newtable(L);
+
+    int row = 1;
+    for (auto r : res) {
+        int col = 1;
+        lua_newtable(L);
+        lua_pushnumber(L, r.start);
+        lua_rawseti(L, -2, col++);
+        lua_pushnumber(L, r.length);
+        lua_rawseti(L, -2, col++);
+        lua_pushnumber(L, r.r);
+        lua_rawseti(L, -2, col++);
+        lua_pushnumber(L, r.g);
+        lua_rawseti(L, -2, col++);
+        lua_pushnumber(L, r.b);
+        lua_rawseti(L, -2, col++);
+
+        lua_rawseti(L, -2, row++);
+    }
+
+    return 1;
+}
+
+int highlight_set_extensions_dir(lua_State* L)
+{
+    const char* p = lua_tostring(L, -1);
+    Textmate::initialize(p);
+    log("highlight_set_extensions_dir");
+    return 1;
+}
+
+int highlight_load_theme(lua_State* L)
+{
+    const char* p = lua_tostring(L, -1);
+    int theme_id = Textmate::load_theme(p);
+    log("highlight_load_theme");
+    lua_pushnumber(L, theme_id);
+    return 1;
+}
+
+int highlight_set_theme(lua_State* L)
+{
+    int id = lua_tonumber(L, -1);
+    Textmate::set_theme(id);
+    return 1;
+}
+
+int highlight_load_language(lua_State* L)
+{
+    const char* p = lua_tostring(L, -1);
+    int lang_id = Textmate::load_language(p);
+    log("highlight_load_language %s %d", p, lang_id);
+    lua_pushnumber(L, lang_id);
+    return 1;
+}
+
+int highlight_set_language(lua_State* L)
+{
+    int id = lua_tonumber(L, -1);
+    Textmate::set_language(id);
+    return 1;
+}
+
+int highlight_add_block(lua_State* L)
+{
+    int linenr = lua_tonumber(L, -2);
+    int docid = lua_tonumber(L, -1);
+
+    if (docs.find(docid) == docs.end()) {
+        return 1;
+    }
+
+    docs[docid]->add_block_at(linenr);
+    return 1;
+}
+
+int highlight_remove_block(lua_State* L)
+{
+    int linenr = lua_tonumber(L, -2);
+    int docid = lua_tonumber(L, -1);
+
+    if (docs.find(docid) == docs.end()) {
+        return 1;
+    }
+
+    docs[docid]->remove_block_at(linenr);
+    return 1;
+}
+
+EXPORT int luaopen_textmate(lua_State* L)
+{
+    Textmate::load_theme_data(THEME_MONOKAI);
+    Textmate::load_language_data(GRAMMAR_CPP);
+
+    lua_newtable(L);
+    lua_pushcfunction(L, highlight_line);
+    lua_setfield(L, -2, "highlight_line");
+    lua_pushcfunction(L, highlight_set_extensions_dir);
+    lua_setfield(L, -2, "highlight_set_extensions_dir");
+    lua_pushcfunction(L, highlight_load_theme);
+    lua_setfield(L, -2, "highlight_load_theme");
+    lua_pushcfunction(L, highlight_load_language);
+    lua_setfield(L, -2, "highlight_load_language");
+    lua_pushcfunction(L, highlight_set_theme);
+    lua_setfield(L, -2, "highlight_set_theme");
+    lua_pushcfunction(L, highlight_set_language);
+    lua_setfield(L, -2, "highlight_set_language");
+    lua_pushcfunction(L, highlight_is_line_dirty);
+    lua_setfield(L, -2, "highlight_is_line_dirty");
+    lua_pushcfunction(L, highlight_make_line_dirty);
+    lua_setfield(L, -2, "highlight_make_line_dirty");
+
+    lua_pushcfunction(L, highlight_add_block);
+    lua_setfield(L, -2, "highlight_add_block");
+    lua_pushcfunction(L, highlight_remove_block);
+    lua_setfield(L, -2, "highlight_remove_block");
+    return 1;
 }
